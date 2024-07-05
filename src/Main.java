25c39e02fbb2adbacb2420e88ccccbbf63585205
Marlie Martinez <marlieahue@gmail.com>
2024-07-05 09:35:22 -0400

    idk

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,36 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        String name = "Marlie";
+        System.out.println(name.charAt(0));
+        if(name.charAt(0) == 'A') {
+            System.out.println("True");
+        }
+            else {
+            System.out.println("Name Does not begin with the letter: A.");
+            System.out.println(escape());
+        }
+    }
+    static int mathOne(){
+        Integer Myinteger = 25;
+        int num = Myinteger;
+        return num;
+    }
+
+    static String stringOne(){
+        //Create and use Strings
+        String marlie = "Marlie";
+        return "";
+    }
+
+    static String escape(){
+        return "\"Marlie\" \nMartinez ";
+    }
+
+    static boolean isCapitalized(char c){
+        if(c == c.toUpperCase()){
+            return true;
+        }
+        System.out.println("hi");
+    }
 }
